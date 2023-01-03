705beb98a60a999cda8ce6a0aafaace5d7cde3e4
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-03 19:27:51 +0100

    Task 0 - 16

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 """Print the alphabet in lowercasei without newline,"""
 
-
-# 2-print_alphabet.py
-# Mallo Ezra
-# ezra.mallo@gmail.com
-
 for letter in range(97, 123):
     print("{}".format(chr(letter)), end="")
