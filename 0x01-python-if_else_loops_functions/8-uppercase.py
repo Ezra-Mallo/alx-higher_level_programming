fc3030621a1bc0092ceca3536919f680f9109069
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-04 00:03:32 +0100

    Task 0 - 16

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,15 +5,15 @@
 
 def uppercase(str):
     """Function that checks for uppercase character."""
-    new_str =""
+    new_str = ""
     lenght = len(str)
 
     for i in range(0, lenght):
         asci = ord(str[i])
 
         if asci >= 97 and asci <= 122:
-            new_str =  new_str + chr(asci - 32)
+            new_str = new_str + chr(asci - 32)
         else:
-            new_str =  new_str + chr(asci)
+            new_str = new_str + chr(asci)
 
     print("{}".format(new_str))
