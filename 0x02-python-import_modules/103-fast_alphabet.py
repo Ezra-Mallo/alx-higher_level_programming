c5146ef25f014e849745b087cd4d284da3219556
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-05 22:24:55 +0100

    Task 0 - 9

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# 103-fast_alphabet.py
+# ezra.mallo@gmail.com
+
+for xter in range(65, 91):
+    print("{}".format(chr(xter)), end="")
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+# 5-variable_load.py
+# ezra.mallo@gmail.com
 
 if __name__ == "__main__":
-    """dasd"""
+    """Program that imports thei variable a from the
+    file variable_load_5.py and prints its value."""
 
     from variable_load_5 import a
 
