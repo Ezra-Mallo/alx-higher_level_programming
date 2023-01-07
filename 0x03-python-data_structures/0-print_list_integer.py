7330a49ce17e80b4eaf0ab9032ffef2a66495f35
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-07 13:10:02 +0100

    Task 0 - 9

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    """Function that prints all integers of a list."""
+
+    for num in my_list:
+        print("{}".format(num), end="\n")
---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """Write a function that retrieves an element from a list like in C."""
+
+    k = 0
+    for num in my_list:
+        if k == idx:
+            return num
+        k += 1
+
