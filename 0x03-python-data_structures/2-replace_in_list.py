073955c6caac513c4c888384fd1c45b519b0e167
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-07 13:55:15 +0100

    Task 0 - 9

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,6 @@
 
 def replace_in_list(my_list, idx, element):
     """Function that replaces an element of a list at a
-
                 specific position (like in C)."""
 
     count = len(my_list)
@@ -11,4 +10,4 @@ def replace_in_list(my_list, idx, element):
         return my_list
     else:
         my_list[idx] = element
-        return my_list
+        return (my_list)
