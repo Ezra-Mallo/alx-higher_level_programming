48a47364083f343e6008d2e68fe30c8327f59d35
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-09 16:43:35 +0100

    Task 0 - 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,7 +6,7 @@
 def max_integer(my_list=[]):
     """Function that finds the biggest integer of a list."""
 
-    if isinstance(my_list, list) == True:
+    if isinstance(my_list, list):
         maximum = 0
         for i in range(len(my_list)):
             if maximum <= my_list[i]:
