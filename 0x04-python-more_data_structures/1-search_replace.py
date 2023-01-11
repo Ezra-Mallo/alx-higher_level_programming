17cb1a2704e0ac0819c8ee2105eb54b8adb0ca4a
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-11 22:42:15 +0100

    Task 0 - 16

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# 1-search_replace.py
+# ezra.mallo@gmail.com
+
+def search_replace(my_list, search, replace):
+    """Search and replaces all."""
+
+
+    new_list = my_list.copy()
+    for i in range(len(new_list)):
+        item = new_list[i]
+        if item == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = item
+
+    return new_list
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 2-uniq_add.py
+# ezra.mallo@gmail.com
+
+
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list."""
+
+    new_list = []
+    for item in my_list:
+        if item not in new_list:
+            new_list.append(item)
+
+    return new_list
