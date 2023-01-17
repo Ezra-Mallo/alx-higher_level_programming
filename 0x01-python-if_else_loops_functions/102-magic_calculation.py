95cee9c13a6464936b11d4e8b0be1d64d4962833
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-17 10:23:30 +0100

    Task 0 - 16

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -4,8 +4,7 @@
 
 def magic_calculation(a, b, c):
     if a < b:
-        return c
+        return (c)
     elif c > b:
         reurn (a + b)
-    else:
-        return (a * b - c)
+    return (a * b - c)
