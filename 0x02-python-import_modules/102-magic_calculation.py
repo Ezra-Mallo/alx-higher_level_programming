f016445cec3c0fce0c737b0951f21dd8649fef18
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-17 10:18:37 +0100

    Task 0 - 9

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 102-magic_calculation.py
+# ezra.mallo@gmail.com
+
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        reurn (a + b)
+    else:
+        return (a * b - c)
