6dc1e3738cbb99554f8c5639a7e2a1c902562613
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-18 18:39:11 +0100

    Task 0 - 16

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,14 +6,12 @@
 def best_score(a_dictionary):
     """Function that returns a key with the biggest integer value."""
 
-    if isinstance(a_dictionary, dict) is False:
-        return ("None")
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
     highest_score = 0
     for index in a_dictionary:
         score = a_dictionary[index]
         if highest_score < score:
             highest_key = index
             highest_score = score
-    if highest_score == 0:
-        return ("None")
     return (highest_score)
