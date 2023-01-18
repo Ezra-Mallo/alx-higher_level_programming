b1665dbb17263df18d698ad5d6ca48b2a486a240
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-19 00:27:26 +0100

    Task 0 - 16

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python3
-# 101-square_matrix_map.py
-# ezra.mallo@gmail.com
-
-
 def square_matrix_map(matrix=[]):
-    """Function that computes the square value
-    of all integers of a matrix using map"""
-
-    if not isinstance(matrix, list) or len(matrix) == 0:
-        return (0)
-
-    result = (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
-    return (result)
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
