4a47a6e4c0c1e11555ca50256acd6075a5c69a2d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-18 09:27:59 +0100

    Task 0 - 9

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -7,6 +7,7 @@ def magic_calculation(a, b):
     """Python function def magic_calculation(a, b):
 
     that does exactly the same as the following Python bytecode:"""
+    from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
