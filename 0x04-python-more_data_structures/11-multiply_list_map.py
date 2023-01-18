4a115b1746c88b709046432266a7a22a55dff82d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-18 20:01:30 +0100

    Task 0 - 16

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,10 @@
+# 11-multiply_list_map.py
+# ezra.mallo@gmail.com
+
+
+def multiply_list_map(my_list=[], number=0):
+    """Function that returns a list with all values
+
+    multiplied by a number without using any loops."""
+
+    return list(map(lambda x: x * number, my_list))
