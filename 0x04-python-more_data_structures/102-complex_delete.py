f5ae493766b2a8d8fda7184996e16a9c1cf3a56c
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-20 11:31:23 +0100

    Task 0 - 16

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 # 102-complex_delete.py
-# ezra.malo@gmail.com
+# ezra.mallo@gmail.com
 
 
 def complex_delete(a_dictionary, value):
     """Function that deletes keys with a specific value in a dictionary."""
 
+    if value is None:
+        return (a_dictionary)
+
     my_list = []
     for k, v in a_dictionary.items():
         if value == v:
             my_list.append(k)
+    if len(my_list) == 0:
+        return (a_dictionary)
+
     for key in my_list:
         if key in a_dictionary:
             del a_dictionary[key]
