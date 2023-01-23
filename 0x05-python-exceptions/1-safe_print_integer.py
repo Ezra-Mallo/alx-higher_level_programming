7ee24e33c5e99e2bbe0af5f8f198794a9ccaeaf8
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-23 16:03:32 +0100

    Task 0 - 10

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-# 0-safe_print_list.py
+# 1-safe_print_integer.py
 # ezra.mallo@gmail.com
 
 
-def safe_print_list(my_list=[], x=0):
-    """Function that prints x elements of a list."""
+def safe_print_integer(value):
+    """Function that prints an integer with "{:d}".format()."""
 
-    num = 0
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end="")
-            num += 1
-        except:
-            break
-
-    print("")
-    return num
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return (False)
