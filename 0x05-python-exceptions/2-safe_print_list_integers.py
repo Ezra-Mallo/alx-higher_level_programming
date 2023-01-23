0a56d612cd17e866525a4c254b0e568abc24cb1f
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-23 18:59:59 +0100

    Task 0 - 10

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    """Function that prints the first x elements of a list and only integers."""
+    """Function to print the first x elements of a list and only integers."""
 
     total = 0
     for i in range(x):
