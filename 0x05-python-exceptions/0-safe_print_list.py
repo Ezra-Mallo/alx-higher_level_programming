861d243ff6f5b064ba6c75a83fab8fac144e92b0
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-23 15:38:09 +0100

    Task 0 - 10

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,11 +5,14 @@
 
 def safe_print_list(my_list=[], x=0):
     """Function that prints x elements of a list."""
-    try:
-        new_val = ""
-        for i in my_list[:x]:
-            new_val += str(i)
-            print("{}".format(new_val))
-        return int(new_val)
-    except ValueError:
-        print(ValueError)
+
+    num = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num += 1
+        except:
+            break
+
+    print("")
+    return num
