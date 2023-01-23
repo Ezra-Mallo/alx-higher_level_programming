3d5080f2a463d1c720ac541f84f1620c3973a81d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-23 15:09:51 +0100

    Task 0 - 10

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 0-safe_print_list.py
+# ezra.mallo@gmail.com
+
+
+def safe_print_list(my_list=[], x=0):
+    """Function that prints x elements of a list."""
+
+    new_val = ""
+    for i in my_list[:x]:
+        new_val += str(i)
+    print("{}".format(new_val))
+    return int(new_val)
