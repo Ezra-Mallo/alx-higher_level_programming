fcff5457b7d9c5da1747e7e0c51629a397e4e7a9
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-24 11:02:52 +0100

    Task 0 - 10

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,7 +8,7 @@ def safe_function(fct, *args):
     """Function that executes a function safely."""
 
     try:
-        result =fct(args[0], args[1])
+        result = fct(args[0], args[1])
         return result
     except Exception as err:
         print("Exception: {}".format(err), file=sys.stderr)
