cb9268302ae81c06f2a205cabae5f11ba2274c9d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-25 16:27:00 +0100

    Task 0 - 10

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,6 +10,11 @@ class Square:
     def __init__(self, size=0):
         """Initialization"""
 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
 
     @property
@@ -25,6 +30,22 @@ class Square:
         if value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
+    @property
+    def position(self):
+        """Retrieves position of square"""
+
+        return self.__position
+
+    @positon.setter
+    def position(self, value):
+        """property setter to set position to a tuple"""
+
+        if not isinstance(value, tuple) and len(value) != 2 and \
+                value[0] < 0 and value[1] < 0
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+        self.__position = value
 
     def area(self):
         """Compute Area"""
