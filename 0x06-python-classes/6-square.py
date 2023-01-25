7428528a83c1e84b055dabbc2a0baf85837b5f46
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-25 17:00:12 +0100

    Task 0 - 10

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,17 +39,18 @@ class Square:
     def position(self, value):
         """property setter to set position to a tuple"""
 
-        #if not isinstance(value, tuple) and len(value) != 2 and \
-        #        value[0] < 0 and value[1] < 0:
-        #    raise TypeError('position must be a tuple of 2 positive integers')
+        if not isinstance(value, tuple) and len(value) != 2 and \
+                not isinstance(value[0], int) and value[0] < 0 and \
+                not isinstance(value[1], int) and value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
 
-        if not isinstance(value, tuple):
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
+        #if not isinstance(value, tuple):
+        #    raise TypeError('position must be a tuple of 2 positive integers')
+        #if len(value) != 2:
+        #    raise TypeError('position must be a tuple of 2 positive integers')
+        #if len([i for i in value if isinstance(i, int) and i >= 0]) != 2:
+        #    raise TypeError('position must be a tuple of 2 positive integers')
+        #self.__position = value
 
         self.__position = value
 
