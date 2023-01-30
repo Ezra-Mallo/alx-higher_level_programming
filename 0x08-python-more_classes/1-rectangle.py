f3ad8678ab8ed9c724abff454e76481e517bf9b8
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-30 17:35:06 +0100

    Task 0 - 10

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+# ezra.mallo@gmail.com
+"""Represent a rectangle Class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    self.width = 0
+
+    def __init__(self, width=0, height=0):
+        """Inititalization"""
+
+        self.width  = width
+        self.height = height
+
+    @property
+    def width(self, value):
+        """Retrieves width of rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def with(self, value):
+        """Property setter for the value for the widht"""
+
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self, value):
+        """Retrieves height of rectangle"""
+
+        return self.__height
+
+
+    @height.setter
+    def height(self, value):
+        """Property setter for the value for the height"""
+
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('iheight must be >= 0')
+        self.__height = value
