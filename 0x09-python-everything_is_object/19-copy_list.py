f58b7ea677e33fda1b2a9b716cbae7654e8c8fbb
Ezra-Mallo <ezra.mallo@gmail.com>
2023-01-31 14:47:21 +0100

    Task 0 - 34

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 19-copy_list.py
+# ezra.mallo@gmail.com
+
+def copy_list(l):
+    """Returns a copy of a list.
+
+    Args:
+        l:List
+    Return: list
+    """
+    my_list =l[:]
+
+
+    return (my_list)
