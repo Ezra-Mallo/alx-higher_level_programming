16e38b6af23ced9c7bb11f011d939344597aa684
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-05 21:04:15 +0100

    Task 0 - 9

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include "Python.h"
+
+/**
+ * print_python_list_info - This function prints some basic info about
+ *              Python lists.
+ * @p: is pointer of type PyObject
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int allocated, size, i;
+	PyListObject *plo;
+
+	plo = (PyListObject *)p;
+	size = plo->ob_base.ob_size;
+	allocated = plo->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
+	for (i = 0; i < size; i++)
+	{
+		printf("Element [%d]: %s\n", i, plo->ob_item[i]->ob_type->tp_name);
+	}
+}
