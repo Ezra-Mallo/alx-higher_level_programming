5cab990a055631cbdc0b596edfdf2813eca71fc7
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-06 22:57:47 +0100

    Task 0 - 13

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# 12-is_same_class.py
+# ezra.mallo@gmail.com
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object = an instance of the specified class
+    or
+
+    Args:
+        obj: Object
+        a_class: the class
+    Returns:
+        True: if is instance
+        False: if not
+
+    otherwise False."""
+
+    if type(obj) == a_class:
+        return (True)
+    return (False)
