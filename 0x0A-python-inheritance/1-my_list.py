3561c12ddf4a3025e59b7ac5624bc8fc5d0b4361
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-07 11:21:08 +0100

    Task 0 - 13

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,16 +5,9 @@
 
 
 class MyList(list):
-    """ A class MyList that inherits from list
-        Args:
-            list
-    """
+    """ A class MyList that inherits from list"""
 
     def print_sorted(self):
-        """Print listed as sorted
-            Args:
-                obj: Objects
-            Returns: List
-        """
+        """Print listed as sorted """
 
         print(sorted(self))
