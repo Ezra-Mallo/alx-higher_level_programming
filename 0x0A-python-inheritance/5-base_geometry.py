56d58720f5292535c9b0eeda7fa2f13680a4dbdf
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-07 12:11:19 +0100

    Task 0 - 13

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 5-base_geometry.py
+# ezra.mallo@gmail.com
+"""Defines an empty class of geometry"""
+
+
+class BaseGeometry:
+    """Emtpy class of BaseGeometry"""
+
+    pass
