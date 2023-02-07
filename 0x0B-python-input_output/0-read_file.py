86cd92e46422a4ce2a1bb81614a9891b62dbc6dc
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-07 16:20:04 +0100

    Task 0 - 13

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# 0-read_file.py
+# ezra.mallo@gmail.com
+"""Defines function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and prints it to stdouti
+
+    Args:
+        filename:
+    """
+
+    with open(filename, "r", encoding="utf-8") as my_file:
+        for line in my_file:
+            print(line, end="")
+
+
+    my_file.closed
