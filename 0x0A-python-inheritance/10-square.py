88779aa4812614ff2486b89eee8a5ed1def61d2f
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-08 15:51:44 +0100

    Task 0 - 13

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -2,13 +2,17 @@
 # 10-square.py
 # ezra.mallo@gmail.com
 """ Defines the instance of square"""
-Rectangle = __import__('9-rectanglei.py').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """The geometric square shapei """
 
-    :wq
+    def __init__(self, size):
+        """Initialisation"""
 
 
-metric 
+        self.__width = self.integer_validator("size", size)
+        self.__height = self.integer_validator("size", size)
+
+        super().__init__(size, size)
