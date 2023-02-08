ae79d828498d9c24254101e65da7475831b6b0b7
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-08 16:34:57 +0100

    Task 0 - 13

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 100-my_int.py
+# ezra.mallo@gmiail.com
+"""Defined int class"""
+
+
+class MyInt(int):
+   """Class that inherits from int"""
+
+   def __eq__(self, other):
+       """Defines behavior for '=='"""
+       return super().__ne__(other)
+
+   def __ne__(self, other):
+       """Defines behavior for '!='"""
+       return super().__eq__(other)
