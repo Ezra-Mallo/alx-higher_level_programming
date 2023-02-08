30886cd91b1b18592dd06c2142680c07ddc0965b
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-08 21:52:24 +0100

    Task 0 - 13

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,12 +5,12 @@
 
 
 class MyInt(int):
-   """Class that inherits from int"""
+    """Class that inherits from int"""
 
-   def __eq__(self, other):
-       """Defines behavior for '=='"""
-       return super().__ne__(other)
+    def __eq__(self, other):
+        """Defines behavior for '=='"""
+        return super().__ne__(other)
 
-   def __ne__(self, other):
-       """Defines behavior for '!='"""
-       return super().__eq__(other)
+    def __ne__(self, other):
+        """Defines behavior for '!='"""
+        return super().__eq__(other)
