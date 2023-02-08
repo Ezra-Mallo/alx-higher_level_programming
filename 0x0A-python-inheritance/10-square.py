e2b276a2a87866eb8838acc81688d0cc35791ccd
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-08 14:14:27 +0100

    Task 0 - 13

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 10-square.py
+# ezra.mallo@gmail.com
+""" Defines the instance of square"""
+Rectangle = __import__('9-rectanglei.py').Rectangle
+
+
+class Square(Rectangle):
+    """The geometric square shapei """
+
+    :wq
+
+
+metric 
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,7 +22,7 @@ class BaseGeometry:
             ValueError: value <= 0
         """
 
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
