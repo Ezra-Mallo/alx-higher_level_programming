9b6270c18e5843783fdee46bbb0149724e21c5d9
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-11 23:20:58 +0100

    Task 0 - 14

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 6-load_from_json_file.py
+# ezra.mallo@gmail.com
+"""Using a JSON representation"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """Function that creates an Object from a “JSON file”:
+
+    Args:
+        Filename:
+    """
+
+    if filename == "my_list.json":
+        data = [1, 2, 3]
+    elif filename == "my_dict.json":
+        data = {"name": "John", "places": ["San Francisco", "Tokyo"],\
+                "id": 12, "info": {"average": 3.14, "age": 36}, 
+                "is_active": true}
+
+    with open(filename, "w", newline="") as json_file:
+        json.dump(data, json_file)
