8ea8c61c463c5d9f72631b7f6440a435c0e1b4eb
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-13 15:45:37 +0100

    Task 0 - 14

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-# 9-student.py
-"""Define Module"""
+# 10-student.py
+"""Defines Module"""
 
 
 class Student:
@@ -12,22 +12,22 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Instatiation
-        Args: 
+        Args:
             first_name(str): first arguemnt
             last_name(str): 2nd arguement
             age (int): 3rd airguement
         Raises: None
         Returns: None
-
-
-
         """
 
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """Return dictionary of the file object"""
 
-        return(self.__dict__)
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        else:
+            return self.__dict__
