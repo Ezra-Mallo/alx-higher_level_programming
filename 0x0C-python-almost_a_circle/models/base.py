4f9788dc5137dae08849cecc7997b0dd8d49c8c6
Ezra-Mallo <ezra.mallo@gmail.com>
2023-02-18 12:11:26 +0100

    Task 0 - 21

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,4 +24,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """JSON the standard formats for sharing data representation."""
-        return (json.dumps(list_dictionaries))
+        if list_dictionaries  == None:
+            return ([])
+        else:
+            return (json.dumps(list_dictionaries))
