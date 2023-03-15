005514bb8434759b890b6e46977010edfeeb0490
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 21:02:30 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,10 +2,10 @@
 --     user_0d_1 should have all privileges on your MySQL server
 --     The user_0d_1 password should be set to user_0d_1_pwd
 --     If the user user_0d_1 already exists, your script should not fail
-CREATE DATABASE user_0d_1;
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+   IF NOT EXISTS 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-    FOR 'user_0d_1'@'localhost'
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
