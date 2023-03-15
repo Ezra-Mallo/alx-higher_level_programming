cc4d84a44975f4f02f87a134c051d6e18d796095
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 01:38:31 +0100

    Task 0 - 20

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,3 @@
 -- Script that creates a table second_table in the database
-CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT) 
-VALUES (1, “John”, 10, 1, “Alex”, 3, “Bob”, 14, 4,“George”,  8);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT),
+VALUES (1, “John”, 10, 2, “Alex”, 3, 3, “Bob”, 14, 4,“George”,  8);
