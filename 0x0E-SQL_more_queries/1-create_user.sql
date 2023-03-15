fbeec8da5308272d6185becdf74ee2b90cadd19b
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 22:25:14 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,9 +5,8 @@
 CREATE USER
    IF NOT EXISTS 'user_0d_1'@'localhost'
    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
+GRANT PRIVILEGES
    ON *.*
    TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd'
    WITH GRANT OPTION;
 FLUSH PRIVILEGES;
