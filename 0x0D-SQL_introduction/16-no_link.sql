38107171051d1e1ef2a0b633652f86f518911a9d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 02:47:16 +0100

    Task 0 - 20

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,4 @@
 -- Script that lists all records of the second_table where name != blank
-
-hat lists the number of records with the same score in second_table
 SELECT `score`, `name`
 FROM `second_table`
 WHERE `name` != NULL
