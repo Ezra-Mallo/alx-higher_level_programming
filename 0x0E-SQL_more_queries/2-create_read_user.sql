6b583b0b7981f80a25490902a3091dcde3698b51
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 22:30:09 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -8,7 +8,7 @@ CREATE DATABASE
 CREATE USER
    IF NOT EXISTS 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT PRIVILEGES SELECT
+GRANT SELECT
    ON `hbtn_0d_2.*`
    TO 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd'
