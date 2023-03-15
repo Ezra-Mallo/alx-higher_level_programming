1294b4aa1d6dd69481e425e96c1beaa17f8b6b02
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-16 00:35:06 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,4 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT `cities`.`id`, `cities`.`name`, `states.name`
   FROM `cities`, `states`
-  ORDER BY `cities`.`id` ASC;
+  ORDER BY `cities`.`id`;
