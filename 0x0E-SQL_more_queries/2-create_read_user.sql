6b2fccab06c3f7c0b098ba1979a5c368e2e7d20d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 22:19:56 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,12 +4,12 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE
-   IF NOT EXISTS 'hbtn_0d_02'
+   IF NOIT EXISTS 'hbtn_0d_02';
 CREATE USER
    IF NOT EXISTS 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT PRIVILEGES SELECT
    ON *.*
    TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd' WITH GRANT OPTION;
+   WITH GRANT OPTION;
 FLUSH PRIVILEGES;
