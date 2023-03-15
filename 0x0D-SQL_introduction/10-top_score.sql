adbfa38864efb55576bcce376652ecd5a131cd98
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 02:08:27 +0100

    Task 0 - 20

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
--- Scripti that lists all records of the table second_of the database
-SELECT `score`, `name` FROM `second_table`;
+-- Scripti that lists all records of the table second_of the databasei
+SELECT `score`, `name` 
+FROM `second_table`
+ORDER BY `score`;
