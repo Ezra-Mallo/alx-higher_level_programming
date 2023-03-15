a6ec10e2fd78c5a740041069e4338ed8e5296020
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 22:27:05 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,7 +4,7 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE
-   IF NOIT EXISTS hbtn_0d_02;
+   IF NOT EXISTS `hbtn_0d_02`;
 CREATE USER
    IF NOT EXISTS 'user_0d_2'@'localhost'
    IDENTIFIED BY 'user_0d_2_pwd';
