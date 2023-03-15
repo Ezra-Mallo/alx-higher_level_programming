5c54d519c1e656da81ff7c9aa44bab07fa55b931
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-16 00:33:17 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,4 +5,4 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT `cities`.`id`, `cities`.`name`, `states.name`
   FROM `cities`, `states`
-  ORDER BY `CITIES`.`id`;
+  ORDER BY `cities`.`id`;
