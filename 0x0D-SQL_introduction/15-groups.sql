101a65d42399cad5d67fc6be7828fc00f87e527c
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 02:42:27 +0100

    Task 0 - 20

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
--- Script that removes all records with a score <= 5 in second_table
+-- Script that lists the number of records with the same score in second_table
 SELECT `score`, count(*) as `number`
 FROM `second_table`
 GROUP BY `score`
