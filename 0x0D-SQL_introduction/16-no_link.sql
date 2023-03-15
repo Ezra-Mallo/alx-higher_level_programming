4e86084a0afe2747de8325077b70a8ec90c810f7
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-15 02:48:15 +0100

    Task 0 - 20

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- Script that lists all records of the second_table where name != blank
 SELECT `score`, `name`
 FROM `second_table`
-WHERE `name` != NULL
+WHERE `name` != ""
 ORDER BY `score` DESC;
