753311d1a5af83b647b7542c71edebf550aa82b2
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-16 23:33:34 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -8,6 +8,6 @@
 -- The database name will be passed as an argument of the mysql command
 SELECT tv_shows.title=NULL, tv_show_genres.genre_id=NULL
     FROM tv_shows
-    INNER JOIN tv_show_genres
+    LEFT JOIN tv_show_genres
     ON tv_shows.id=tv_show_genres.show_id
-    ORDER BY tv_shows.title, tv_show_genres.genre_id;
+    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
