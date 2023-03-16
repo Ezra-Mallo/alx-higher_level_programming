5be50fb6fe486b127370bff98be2795a0859a1b1
Ezra-Mallo <ezra.mallo@gmail.com>
2023-03-16 22:43:31 +0100

    Task 0 - 18. (project 0x0E. SQL - More queries)

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,6 +5,6 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SEL:ECT `cities`.`id`, `cities`.`name`, `states.name`
-  FROM `cities`, `states`
-  ORDER BY `cities`.`id`;
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows.title, tv_show_genres.genre_id
+    ORDER BY tv_shows.title, tv_show_genres.genre_id
