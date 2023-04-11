061be7a37642f263d627f189c8b9df536ed38686
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-11 22:50:23 +0100

    Task 5. An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+/*
+Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+  If the argument can’t be converted to an integer, print “Not a number”
+  You must use console.log(...) to print all output
+  You are not allowed to use var
+  You are not allowed to use try/catch
+ */
+
+const process = require('process');
+const myVar = process.argv;
+
+if (typeof (myVar[2]) !== 'undefined') {
+  arg = parseInt(myVar[2]);
+
+  if ((typeof (arg === 'number')) || (arg !== 'NaN')) {
+    console.log('My number: ' + arg);
+  } else {
+    console.log('Not a number');
+  }
+}
