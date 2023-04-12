0d14e9690deab163b01a590af9641a740d717685
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-12 08:33:39 +0100

    Task 10. Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -14,7 +14,7 @@ function factorial (n) {
 
   if (n < 0) {
     return (-1);
-  } else if (n === 0) {
+  } else if ((n === 0) || (n === 'NaN')) {
     return (1);
   } else {
     x = n * factorial(n - 1);
