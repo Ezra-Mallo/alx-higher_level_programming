c603e9f52bb6eeceea5fd5cf65e734f0915c4f55
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-12 08:59:48 +0100

    Task 13. Add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,4 +1,18 @@
 #!/usr/bin/node
-function add(a, b) {
-  return (a+b);
-}
+/*
+ Write a function that returns the addition of 2 integers.
+   The function must be visible from outside
+   The name of the function must be add
+   You are not allowed to use var
+*/
+
+exports.add = function add (a, b) {
+  return (a + b);
+};
+
+/*
+    OR
+
+exports.add = (a, b) => a + b;
+
+ */
