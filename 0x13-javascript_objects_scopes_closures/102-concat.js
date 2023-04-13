a72678871a601eeb4ec13874f04be095b4011f75
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-13 09:24:28 +0100

    Task 13. Concat files

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/*
+ Write a script that concats 2 files.
+   The first argument is the file path of the first source file
+   The second argument is the file path of the second source file
+   The third argument is the file path of the destination
+ */
+
+const fs = require('fs');
+const readline = require('readline');
+
+const firstSrcFile = 'fileA';
+const secondSrcFile = 'fileB';
+const destFile = 'fileC';
+
+const inputStreamA = fs.createReadStream(firstSrcFile);
+const inputStreamB = fs.createReadStream(secondSrcFile);
+const outputStream = fs.createWriteStream(destFile, { encoding: 'utf8' });
+
+const lineReaderA = readline.createInterface({
+  input: inputStreamA, terminal: false
+});
+
+const lineReaderB = readline.createInterface({
+  input: inputStreamB, terminal: false
+});
+
+lineReaderA.on('line', function (line) {
+  outputStream.write(line + '\n');
+});
+
+lineReaderB.on('line', function (line) {
+  outputStream.write(line + '\n');
+});
