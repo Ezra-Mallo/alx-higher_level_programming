6312429e70bcd7b7963b464b682fbf1d82c6877f
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-17 20:22:29 +0100

    Updated- Task 2. Filter states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,15 +6,10 @@
 from sys import argv
 import MySQLdb
 
-my_port = 3306
-my_host = "localhost"
-
 if __name__ == "__main__":
     """    Connect to database, query and display states from the database."""
-    db = MySQLdb.connect(host=my_host, port=my_port,
-                         user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db.cursor()
     db_cursor.execute("SELECT * FROM states WHERE name = '{}'".format(argv[4]))
-
     for state in db_cursor.fetchall():
         print(state)
