f03c3c02895da7cb78cdb864e58f6908b9d5148e
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-17 18:01:34 +0100

    Task 1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -14,13 +14,11 @@ my_port = 3306
 my_host = "localhost"
 
 if __name__ == "__main__":
-    """    Connect to database, query and display states from the database."""
     db = MySQLdb.connect(host=my_host, port=my_port,
                          user=argv[1], passwd=argv[2], db=argv[3])
-
     db_cursor = db.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'
-                      ORDER BY 'state.id'")
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+                      ORDER BY 'id'")
 
     for state in db_cursor.fetchall():
         print(state)
