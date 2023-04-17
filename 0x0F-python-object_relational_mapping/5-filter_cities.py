6b8c965fe598d57a71fe63b0804a75985f9be257
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 00:48:50 +0100

    Task 4. Cities by states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,10 +12,8 @@ if __name__ == "__main__":
                          user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db.cursor()
     db_cursor.execute("SELECT cities.id, cities.name, states.name\
-                      FROM cities\
-                      INNER JOIN states\
-                      ON cities.state_id = states.id\
-                      ORDER BY cities.id ASC")
+                      FROM cities, states\
+                      WHERE cities.state_id=states.id ORDER BY cities.id ASC")
     for state in db_cursor.fetchall():
         print(state)
     db.close()
