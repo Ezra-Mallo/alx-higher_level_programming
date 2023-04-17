02e26cac5173c25b6a4d4e0249d68f48f67bef6c
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-17 22:12:40 +0100

    Task 3. SQL Injection...

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db.cursor()
-    db_cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    db_cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                      ORDER BY id")
     for state in db_cursor.fetchall():
         print(state)
