7a514cf79481f50e4d02ffa9650d79419bda5209
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 01:05:17 +0100

    Task 5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,14 +12,13 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     db_cursor = db.cursor()
-    my_query = "SELECT cities.name\
+
+    db_cursor.execute("SELECT cities.name\
                       FROM cities\
                       INNER JOIN states\
                       ON cities.state_id = states.id\
-                      WHERE states.name = %s\
-                      ORDER BY cities.id ASC"
-
-    db_cursor.execute(my_query, (my_search, ))
+                      WHERE states.name = {}\
+                      ORDER BY cities.id ASC".format(my_search))
     for state in db_cursor.fetchall():
         print(state)
     db.close()
