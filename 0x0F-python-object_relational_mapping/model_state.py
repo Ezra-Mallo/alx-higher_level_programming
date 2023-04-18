44751e7107504de48899aaaaed3cb744ced31eb2
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 11:43:47 +0100

    Task 6. First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from sqlalchemy import create_engine, Column, Integer, String
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 # Create instance of declarative base
 Base = declarative_base()
