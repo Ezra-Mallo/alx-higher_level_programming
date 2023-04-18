fbf25e177887d62a0aa1977eb81dd1cde4333bc3
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 12:56:56 +0100

    Task 7. All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -6,6 +6,7 @@ a Base class to work with MySQLAlchemy ORM.
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
