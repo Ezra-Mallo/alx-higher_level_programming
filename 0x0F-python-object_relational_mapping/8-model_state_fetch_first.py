d571142b6dba41657bae79d786410560a6786fb8
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 14:46:19 +0100

    Task 7. All states via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM.
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    my_session = Session()
+
+    result = my_session.query(State).order_by(State.id).first()
+
+    if result is None:
+        print("Nothing")
+    else:
+        print("{}: {}".format(result.id, result.name))
