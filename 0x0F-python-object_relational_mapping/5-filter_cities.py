c2137ce2894f84dc88613d85d09e397f45b88888
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-18 10:24:30 +0100

    Task 5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     my_query = "SELECT cities.name\
             FROM cities\
             INNER JOIN states ON cities.state_id = states.id\
-            WHERE states.name LIKE BINARY %s\
+            WHERE states.name = %s\
             ORDER BY cities.id"
     db_cursor.execute(my_query, (my_search, ))
     result = list(db_cursor.fetchall())
