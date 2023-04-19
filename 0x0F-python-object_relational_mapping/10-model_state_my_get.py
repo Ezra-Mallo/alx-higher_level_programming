1911931dd3d6dc11e337f2bf9f95bc7778358277
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-19 14:17:01 +0100

    Task 10. Get a state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" This script calls the object Base state and makes a session to
-query the Base class to dislay first record. It work with MySQLAlchemy ORM."""
+""" This script prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa"""
 
 import sys
 from model_state import Base, State
@@ -15,8 +15,10 @@ if __name__ == "__main__":
 
     Session = sessionmaker(bind=engine)
     my_session = Session()
-    my_query = my_session.query(State).filter(State.name
-                                              .like('%s')).order_by(State.id)
-
-    for state in my_query.all():
-        print("{}: {}".format(state.id, state.name))
+    my_query = my_session.query(State).filter(State.name == argv[4])
+    .order_by(State.id)
+    if my_query.count() is None:
+        print("Nothing found")
+    else:
+        for state in my_query.all():
+            print("{}: {}".format(state.id))
