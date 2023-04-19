4929ba63306003ed201df371e7a7286b252e67e7
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-19 22:12:08 +0100

    Task 13. Delete states

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This class makes reference to Base model of City)"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import Column, Interger, PrimaryKey, String, ForeignKey
+from sqlalchemy import.ext.:
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" This script creaes a class for new State model"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City class
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
+    """
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False, foreign_key)
