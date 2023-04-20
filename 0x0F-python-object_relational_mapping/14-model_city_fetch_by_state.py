62af03a7da6be1068c730c8888a30eb850a31d10
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-20 15:48:38 +0100

    Task 14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,13 +9,14 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """Setting up the session engine"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    if len(argv) <= 4:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                               .format(argv[1], argv[2], argv[3]),
+                               pool_pre_ping=True)
 
-    Session = sessionmaker(bind=engine)
-    my_session = Session()
-    my_query = my_session.query(City, State).join(State).order_by(City.id)
+        Session = sessionmaker(bind=engine)
+        my_session = Session()
+        my_query = my_session.query(City, State).join(State).order_by(City.id)
 
-    for city, state in my_query:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+        for city, state in my_query:
+            print("{}: ({}) {}".format(state.name, city.id, city.name))
