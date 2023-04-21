6851927f1be7745815e99a0c654960b76c7aa78e
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-21 16:00:14 +0100

    Updated Task5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,21 +8,22 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    my_search = argv[4]
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db.cursor()
-    my_query = "SELECT cities.name FROM cities\
-            INNER JOIN states ON cities.state_id = states.id\
-            WHERE states.name = %s ORDER BY cities.id"
-    db_cursor.execute(my_query, (my_search, ))
-    result = list(db_cursor.fetchall())
-    count = len(result)
-    if count >= 1:
-        i = 1
-        for cities in result:
-            if (i < count):
-                print(cities[0], end=", ")
-            else:
-                print(cities[0])
-            i += 1
+    if (len(argv) == 5):
+        my_search = argv[4]
+        db = MySQLdb.connect(host="localhost", port=3306,
+                             user=argv[1], passwd=argv[2], db=argv[3])
+        db_cursor = db.cursor()
+        my_query = "SELECT cities.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id"
+        db_cursor.execute(my_query, (my_search, ))
+        result = list(db_cursor.fetchall())
+        count = len(result)
+        if count >= 1:
+            i = 1
+            for cities in result:
+                if (i < count):
+                    print(cities[0], end=", ")
+                else:
+                    print(cities[0])
+                i += 1
