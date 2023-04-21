e5551e96b0ef5482eb176297162dd668f347ecae
Ezra-Mallo <ezra.mallo@gmail.com>
2023-04-21 16:10:04 +0100

    Updated Task 14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,7 +9,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     """Setting up the session engine"""
-    if len(argv) <= 4:
+    if len(argv) == 4:
         engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                                .format(argv[1], argv[2], argv[3]),
                                pool_pre_ping=True)
