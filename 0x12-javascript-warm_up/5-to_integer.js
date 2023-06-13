196b03a72d7d92ec66fbf3f2ffab707396659f9e
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-13 20:37:31 +0100

    Updated Task-5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -10,8 +10,8 @@ You are not allowed to use try/catch
 
 const process = require('process');
 
-if (Number.isInteger(parseInt(process.argv[2])) !== true) {
-    console.log("Not a number");
-} else {
+if (parseInt(process.argv[2])) {
     console.log("My number:", parseInt(process.argv[2]));
+} else {
+    console.log("Not a number");
 }
