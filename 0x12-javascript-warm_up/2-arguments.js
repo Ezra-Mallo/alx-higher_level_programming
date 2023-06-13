9e66ad12524f641efd2e836962d5f71c6e0a3015
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-13 19:04:24 +0100

    Added Task-2. Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 /*
- * A script that prints a message depending of the number of arguments passed:
+  A script that prints a message depending of the number of arguments passed:
+  If no arguments are passed to the script, print “No argument”
+  If only one argument is passed to the script, print “Argument found”
+  Otherwise, print “Arguments found”
+  You must use console.log(...) to print all output
+  You are not allowed to use var
+  Reference: process.argv
  */
 
 const process = require('process');
 const myVar = process.argv;
 const varCount = process.argv.length - 2;
+
 if (varCount === 0) {
   console.log('No argument');
 } else if (varCount === 1) {
