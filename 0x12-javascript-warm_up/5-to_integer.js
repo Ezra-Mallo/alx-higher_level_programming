06f28f017c8004fa0895ceadd7f463904c78b9a9
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-13 20:42:14 +0100

    Updated Task-5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 /*
-Write a script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer:
+A script that prints My number: <first argument converted in integer>
+if the first argument can be converted to an integer:
 
 If the argument can’t be converted to an integer, print “Not a number”
 You must use console.log(...) to print all output
@@ -10,7 +11,7 @@ You are not allowed to use try/catch
 
 const process = require('process');
 
-if (parseInt(process.argv[2])) {
+if (Number.isInteger(parseInt(process.argv[2]))) {
     console.log("My number:", parseInt(process.argv[2]));
 } else {
     console.log("Not a number");
