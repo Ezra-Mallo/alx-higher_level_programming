8a1f7cba0dc2e7f152c4bdf6e843bef62a8e5547
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-13 19:38:34 +0100

    Added Task-3. Value of my argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+/*
+   A script that prints the first argument passed to it:
+   If no arguments are passed to the script, print “No argument”
+   You must use console.log(...) to print all output
+   You are not allowed to use var
+   You are not allowed to use length
+ */
+
+const process = require('process');
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
