c0a41d94e1bfa2d77133b1bfa23134ef08623bc5
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-14 01:26:28 +0100

    Updated Task 15. Call me Moby

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -9,7 +9,7 @@ You are not allowed to use var
 exports.callMeMoby = function callMeMoby (x, theFunction) {
   if (x > 0) {
     console.log('C is fun');
-    x -= 1;
-    callMeMoby(x, theFunction);
+    x = x - 1;
+    callMeMoby(x, callMeMoby)
   }
 };
