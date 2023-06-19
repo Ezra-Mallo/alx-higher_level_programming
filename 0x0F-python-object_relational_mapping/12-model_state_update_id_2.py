0b46f1ef5a7771a0666801bf1c83533e6373c155
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-19 23:23:44 +0100

    Update Task 12. Update a state

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -27,7 +27,11 @@ if __name__ == "__main__":
     my_session = Session()
 
     my_query = my_session.query(State).filter(State.id == 2)
-    my_query.name = "New Mexico"
+    if my_query.count() != 0:
+        for state in my_query:
+            state.name = "New Mexico"
+            my_session.commit()
+            
     my_session.commit()
 
     my_session.close()
