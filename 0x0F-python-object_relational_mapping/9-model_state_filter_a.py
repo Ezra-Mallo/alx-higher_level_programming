3baf787faf89057e705d3b75e250e2bbc3b88a6c
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-19 21:59:23 +0100

    Added Task 9. Contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -18,6 +18,7 @@ from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
     db_engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
