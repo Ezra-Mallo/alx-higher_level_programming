e045bee7b9e85ec465b54f25b377b999c818ce02
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-20 08:37:16 +0100

    Added Task 14. Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     """Setting up the session engine"""
     if len(argv) == 4:
