473370eddc55f509be2da0b03623c8b5ca01cb97
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-20 08:43:40 +0100

    Updated Task 14. Cities in state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -15,7 +15,7 @@ class City(Base):
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
     state = relationship("State")
