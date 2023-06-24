f9afd20ec4784f877f7468f5cead7e09b399d8e0
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-24 13:47:33 +0100

    Updated Task 16. List relationship

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -8,7 +8,7 @@ import sys
 
 
 if __name__ == "__main__":
-    db_engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+    db_engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
                               format(argv[1], argv[2], argv[3]),
                               pool_pre_ping=True)
     Session = sessionmaker(bind=db_engine)
