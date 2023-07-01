9f02098ff07f49adea4c4c9a14a1734d2f3da754
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-02 00:54:14 +0100

    Updated Task 5, 7 and 8

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to POST to header email as test@gmail.com & subject as "I will always be here for PLD"
-curl -sd POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -s POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
