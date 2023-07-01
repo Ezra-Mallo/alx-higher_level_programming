389912199e4dd501c8b7aec79d0df78348230c2d
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-01 15:23:26 +0100

    Task 2 and 3

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that send request to that URL and displays the size of the body in byte
+# Script that sends a DELETE request to the URL passed as the first argument and displays the body of the response
 curl -sX DELETE "$1"
