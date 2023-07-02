f94f4655107e5e7b8c71c9c4523a5b8d4b95a8ff
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-02 01:55:00 +0100

    Updated Task 0 & 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,13 @@
 # Write a Python script that fetches https://alx-intranet.hbtn.io/status
 
 import urllib.request
+
+
 if __name__ == "__main__":
-    from urllib.request import urlopen, Request
-
-    url = "https://alx-intranet.hbtn.io/status"
-    my_req = Request(url)
-    with urlopen(my_req) as respons:
-        content = response.read()
-
-    print("Body response:")
-    print("\t- type: {}".format{type(content)}
-    print("\t- content: {}".format(content))
-    print("\t- utf8 content: {}",format(content.decode('utf-8')))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
+"""Script that takes in a URL, sends a request to the URL and displays the
+   value of the X-Request-Id variable found in the header of the response.
+   
+   Usage: ./1-hbtn_header.py <URL>
 """
-from sys import argv
+import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = argv[1]
+    url = sys.argv[1]
 
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
