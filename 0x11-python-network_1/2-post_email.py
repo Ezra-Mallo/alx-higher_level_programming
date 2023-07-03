0d386afb1d3f2c75d1c6e34aa201a2b88415bfcd
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 01:36:02 +0100

    Updated Task 2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,14 +9,11 @@ import urllib.parse
 
 if __name__ == "__main__":
 
-    count = len(argv)
-    if count == 3:
-        url = argv[1]
-        email = argv[2]
-        values = {'email': email}
-
-        data = urllib.parse.urlencode(values)
-        data = data.encode('ascii')
-        req = urllib.request.Request(url, data)
-        with urllib.request.urlopen(req) as response:
-            print(response.read().decode("utf-8"))
+    url = argv[1]
+    email = argv[2]
+    values = {'email': email}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
