edf9b0bdc5d2e144a19bbeb5c56949cdc203370a
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 12:26:54 +0100

    Added Task 8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) == 0:
+        my_letter = ""
+    else:
+        my_letter = argv[1]
+
+    value = {"q": my_letter}
+    r = requests.post(url, data=value)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
