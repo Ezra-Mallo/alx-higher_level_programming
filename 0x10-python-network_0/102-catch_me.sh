aa0b4e98b1ad9f97b66f332a639a42b5ce7e82cb
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 10:18:24 +0100

    Updated Task 9. Catch me if you can!

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+
+
+##!/bin/bashi
+## catch me if you can
+#curl -s -X PUT 0.0.0.0:5000/catch_me -o /dev/null -w "%{redirect_url}"| xargs curl -sL -X PUT -H 'Origin:School' -F 'user_id= 98'
