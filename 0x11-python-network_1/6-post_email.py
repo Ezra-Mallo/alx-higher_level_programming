e0bcfd2083d086597a22b6df2f2a16c9259d733e
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 03:10:46 +0100

    Added Task 6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8)."""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    r = requests.post(url, data={'email': argv[2]})
+    print(r.text)
