72fde8cff89c090c9e6ffaf3dd9dcc51aa0505cd
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 02:27:10 +0100

    Added Task 4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,20 +2,11 @@
 """Script that takes in a URL, sends a request to the URL and displays the
 body of the response (decoded in utf-8)."""
 
-from sys import argv
-import urllib.request
-import urllib.error
+import requests
 
 
 if __name__ == "__main__":
-
-    url = 'https://alx-intranet.hbtn.io/status'
-    req = urllib.request.Request(url)
-    try:
-        with urllib.request.urlopen(req) as response:
-            body = response.read()
-            print("Body response:")
-            print("\t- type: {}".format(type(body)))
-            print("\t- content: {}".format(body))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
