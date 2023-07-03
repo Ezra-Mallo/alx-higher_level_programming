a86fb9563b1734adb3d73bdad350ba4990b7aee8
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-03 01:55:51 +0100

    Added Task 3. Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""Script that takes in a URL and an email, sends a POST request to the passed
-   URL with the email as a parameter, and displays the body of the response
-   (decoded in utf-8)"""
+"""Script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8)."""
+
 from sys import argv
 import urllib.request
-import urllib.parse
+import urllib.error
 
 
 if __name__ == "__main__":
 
-    count = len(argv)
-    if count == 3:
-        url = argv[1]
-        email = argv[2]
-        values = {'email': email}
-
-        data = urllib.parse.urlencode(values)
-        data = data.encode('ascii')
-        req = urllib.request.Request(url, data)
+    url = argv[1]
+    req = urllib.request.Request(url)
+    try:
         with urllib.request.urlopen(req) as response:
-           the_page = response.read()
-           print(response.read().decode("utf-8"))
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print(e.reason)
