4e87aec3307b8cea27fad60e7185cdac865b814c
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-09 19:03:37 +0100

    Update task 9. My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""  A Python script that takes your GitHub credentials (username and password) 
+"""  A Python script that takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id"""
 from sys import argv
 from requests import get
